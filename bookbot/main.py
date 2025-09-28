e06adf9d53e7e61d7d3aad2b10bdec3aba5f7269
ndubs18 <nick.winkler1@gmail.com>
2025-09-28 06:37:41 -0700

    commmand line args

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,13 +1,15 @@
+import sys
 from stats import count_words, count_chars, sort_counts \
     ,print_sorted_report
 
-BOOK_PATH = 'books/frankenstein.txt'
-
 def main():
-
+    if len(sys.argv) < 2:
+        print('python3 main.py <path_to_book>')
+        sys.exit(1)
+    bookPath = sys.argv[1]
     print('============ BOOKBOT ============');
-    print(f'Analyzing book found at {BOOK_PATH}...')
-    text = get_book_text(BOOK_PATH);
+    print(f'Analyzing book found at {bookPath}...')
+    text = get_book_text(bookPath);
     wordCount = count_words(text);
 
     print('----------- Word Count ----------')
@@ -18,8 +20,15 @@ def main():
     print_sorted_report(sortedCounts);
 
 def get_book_text(path):
-    with open(path, 'r', encoding='utf-8') as text:
-        return text.read()
+    try:
+        with open(path, 'r', encoding='utf-8') as text:
+            return text.read()
+    except FileNotFoundError:
+        print('File not found')
+    except PermissionError:
+        print("You don't have permission to read this file.")
+    except Exception as e:
+        (f'An unexpected error occurred: {e}')
 
 # entrypoint
 main();
